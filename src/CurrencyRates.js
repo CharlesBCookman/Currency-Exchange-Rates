d77f38dea9a3d3b2b813f2855beb0183d37d7d48
CharlesBCookman <CharlesBCookman@gmail.com>
2023-02-10 11:07:50 -0800

    pushing site to remote repository and getting API call set up

--- src/CurrencyRates.js
+++ src/CurrencyRates.js
@@ -1,1 +1,19 @@
 //Business Logic
+
+export default class CurrencyRatesFor {  
+  static getTop5() {
+    
+    return fetch(`https://v6.exchangerate-api.com/v6/57add13920ab87b9e1d0b539/latest/USD`)
+      .then(function(response) {
+        if (response.result !== "success") {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })      
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
+//UI logic
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Character from './Template.js';
+import Music from './CurrencyRates.js';
 
-//UI logic
+function getExchangeRates() {
+  Music.getTop5()
+    .then(function (response) {
+      if (response.data) {
+        printElements(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
+function printElements(response) {
+  for (let i = 0; i < response.data.length; i++) {
+    let pEl = document.createElement("p");
+    pEl.innerText = response.data[i].title;
+    document.querySelector("#top5-div").append(pEl);
+  }
+}
+
+function printError() {
+  alert('something went wrong');
+}
+
+function handleFormSubmission() {
+  getMusic();
+}
+
+window.addEventListener("load", function () {
+  if (window.location.href.match('top5.html') != null) {
+    handleFormSubmission();
+
+  }
+
+});
+
